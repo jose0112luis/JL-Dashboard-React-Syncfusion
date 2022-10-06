c804b349f0ea42d7a36ada93df7222f4c9ad224f
Jose0112Luis <jose0112luis@hotmail.com>
2022-10-06 11:20:06 -0500

    Página de Pedidos, tabla con filtro, paginación y búsqueda.

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, Toolbar, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
+
+  const toolbarOptions = ['Search'];
+
   return (
-    <div>Orders</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders' />
+
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging={true}
+        allowSorting={true}
+        pageSettings={{ pageSize: 15, pageCount: 3 }}
+        sortSettings={contextMenuItems}
+        toolbar={toolbarOptions}
+        enableStickyHeader={true}
+        enableHover={true}
+      >
+        <ColumnsDirective>
+          {
+            ordersGrid.map((item, index) => (
+              <ColumnDirective key={index} {...item} />
+            ))
+          }
+        </ColumnsDirective>
+        <Inject services={[ Toolbar, Page, Sort, Resize, ContextMenu, Filter, ExcelExport, PdfExport, Edit ]} />
+      </GridComponent>
+    </div>
   )
 }
 
