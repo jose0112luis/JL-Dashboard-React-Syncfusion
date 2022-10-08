0d04e6bf41a3dd53f9d07514efefdbd7c318abd4
Jose0112Luis <jose0112luis@hotmail.com>
2022-10-08 15:46:24 -0500

    Creando Gráfico de Área con tema light/dark.

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, SplineAreaSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
 
 const Area = () => {
+
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Area</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Area' />
+
+      <div className='w-full dark:bg-secondary-dark-bg'>
+        <ChartComponent
+          id='area-chart'
+          height='420px'
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          tooltip={{ enable: true }}
+          chartArea={{ border: { width: 1 } }}
+          theme={currentMode === 'Light' ? 'Material' : 'TailwindDark'}
+        >
+          <Inject services={[ SplineAreaSeries, DateTime, Legend, Tooltip ]} />
+
+          <SeriesCollectionDirective>
+            {
+              areaCustomSeries.map((item, index) => (
+                <SeriesDirective key={index} {...item} />
+              ))
+            }
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
