c0587509728d7a391490717260eda1ffad9948cf
Jose0112Luis <jose0112luis@hotmail.com>
2022-10-08 17:14:45 -0500

    Creando Gráfico de Pirámide con tema light/dark.

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, AccumulationLegend, AccumulationDataLabel, AccumulationTooltip, PyramidSeries, AccumulationSelection } from '@syncfusion/ej2-react-charts';
+
+import { PyramidData } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
 
 const Pyramid = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pyramid</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Chart' title='Pyramid' />
+
+      <div className="w-full">
+        <AccumulationChartComponent
+          id='pyramid-chart'
+          tooltip={{ enable: true }}
+          theme={currentMode === 'Dark' ? 'TailwindDark' : 'Material'}
+        >
+          <Inject services={[ AccumulationDataLabel, AccumulationTooltip, PyramidSeries, AccumulationLegend, AccumulationSelection ]} />
+
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective 
+              name='Food'
+              dataSource={PyramidData}
+              xName='x'
+              yName='y'
+              type='Pyramid'
+              width='45%'
+              height='80%'
+              neckWidth='15%'
+              gapRatio={0.03}
+              explode={true}
+              emptyPointSettings={{ mode: 'Drop', fill: 'red' }}
+              dataLabel={{
+                visible: true,
+                position: 'Inside',
+                name: 'text',
+              }}
+            />
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+      </div>
+    </div>
   )
 }
 
