466eff2eac9db554f9ccf032cb9e75019e03fae3
Jose0112Luis <jose0112luis@hotmail.com>
2022-10-13 21:18:56 -0500

    Mejorando componente Button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Button = ({ color, bgColor, size, text, borderRadius }) => {
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ color, bgColor, size, text, borderRadius, icon, bgHoverColor, width }) => {
+  
+  const { setIsClicked, initialState } = useStateContext();
+
   return (
     <button
       type='button'
+      onClick={() => setIsClicked(initialState)}
       style={{ backgroundColor: bgColor, color, borderRadius }}
-      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      className={`text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
-      { text }
+      {icon} { text }
     </button>
   )
 }
